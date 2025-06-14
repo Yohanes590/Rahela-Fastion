3cffc87859e9b5ab2809b22d591fa96e71b7e334
Yohanes Mulugeta <jplussince34@gmail.com>
2025-06-14 16:42:24 +0300

    photo changes

--- components/photo-component.tsx
+++ components/photo-component.tsx
@@ -5,18 +5,18 @@ export default function PhotoGrid() {
             <div className="h-[auto] flex flex-wrap  w-[100%]">
                   
                   <div className="gird-part justify-between w-[100%] flex gap-5  flex-wrap">
-  <div className="each-grid-photo">
-                  <Image src="/assets/grid-5.jpeg" alt="Grid photo" width={500} height={100} className="object-cover"/>  
+  <div className="each-grid-photo w-[400px] h-[550px] overflow-hidden">
+                  <Image id="horizontal-1" src="/assets/grid-5.jpeg" alt="Grid photo" width={400} height={100} className="object-cover"/>  
             </div>
 
                   
-                        <div className="each-grid-photo">
-                  <Image src="/assets/grid-3.jpeg" alt="Grid photo" width={500} height={100} className="object-cover w-[850px] h-[365px]"/>  
-                  <Image src="/assets/grid-2.jpeg" alt="Grid photo" width={500} height={100} className="object-cover mt-[20px] w-[850px] h-[365px]"/>  
+                        <div className="each-grid-photo w-[950px] h-[550px] overflow-hidden">
+                  <Image id="mid-1" src="/assets/grid-3.jpeg" alt="Grid photo" width={500} height={100} className="object-cover w-[950px] h-[265px]"/>  
+                  <Image id="mid-2" src="/assets/grid-2.jpeg" alt="Grid photo" width={500} height={100} className="object-cover mt-[20px] w-[950px] h-[265px]"/>  
                   </div>
                   
-                                 <div className="each-grid-photo">
-                  <Image src="/assets/grid-1.jpeg" alt="Grid photo" width={500} height={100} className="object-cover"/>  
+                                 <div className="each-grid-photo w-[400px] h-[550px] overflow-hidden">
+                  <Image id="horizontal-2" src="/assets/grid-1.jpeg" alt="Grid photo" width={400} height={100} className="object-cover"/>  
                   </div>
                   
 
