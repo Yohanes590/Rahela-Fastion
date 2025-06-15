a4c2fb78b335b37a2744ef80b2654dce95ad0372
Yohanes Mulugeta <jplussince34@gmail.com>
2025-06-15 22:33:48 +0300

    about-and-style

--- components/about-component.tsx
+++ components/about-component.tsx
@@ -5,8 +5,8 @@ export default function AboutComponent() {
             <div className="h-[700px] w-[100%] bg-[#F5F5F5] flex flex-wrap  justify-around items-center">
 
                   <div className="about-message-container text-center">
-                        <h1 className="text-[50px] text-[#918F8E]">Who we are</h1>
-                        <p className="w-[600px] text-[18px]">Rahela offers versatile, earth-toned pieces celebrating African tribes and
+                        <h1 className="text-[60px] text-[#918F8E]">Who we are</h1>
+                        <p className="w-[650px] text-[20px]">Rahela offers versatile, earth-toned pieces celebrating African tribes and
                         cultures.The brand promotes sustainable and ethical fashion practices.
                               It aims to make fashion accessible while bridging Ethiopia with Africa.</p>
                         <center>
--- components/card-component.tsx
+++ components/card-component.tsx
@@ -3,67 +3,23 @@ import Image from "next/image"
 export default function CardComponent() {
       return <>
 
-            <div className="card-container bg-[linear-gradient(to_bottom,#252120_0px,#252120_730px,transparent_400px)] w-full text-[white] h-auto">
-                  <center>
-                  <div className="card-content-headers w-[650px] pt-[50px] text-center">
+            <div className="card-container h-screen ">
+                  <div className="left-text text-right pr-[40px] pt-[70px] text-[#686464] text-[35px]">Comfortable, everyday wear!</div>
+                  <div className="flex-photos flex justify-center items-center gap-3 mt-[50px]">
+                        <div className="each-img-hover w-[400px] h-[600px] overflow-hidden cursor-pointer">
+                        <Image src='/assets/grid-5.jpeg' alt="flex image's" width={400} height={400}/>
+                        </div>
+                        <div className="each-img-hover w-[400px] h-[600px] overflow-hidden cursor-pointer">
+                              <Image src='/assets/grid-3.jpeg' alt="flex image's" width={400} height={400} />
+                        </div>
+                        <div className="each-img-hover w-[400px] h-[600px] overflow-hidden cursor-pointer">
+                              <Image src='/assets/grid-2.jpeg' alt="flex image's" width={400} height={400} />
+                        </div>
+                              <div className="each-img-hover w-[400px] h-[600px] overflow-hidden cursor-pointer">
+                              <Image src='/assets/grid-1.jpeg' alt="flex image's" width={400} height={400} />
+                        </div>
                         
-                        <h1 className="text-[#FFA883] font-bold text-[40px]" style={{ fontFamily: 'Acumin Pro' }}>Our Core Business</h1>
-                        <center>
-                        <h2 className="text-[37px] w-[500px]" style={{ fontFamily: 'Acumin Pro' }}>Blending traditional African 
-                                    patterns with modern design.</h2>
-                              </center>
-                        <p className="mt-[10px] text-[#B5B5B5]">Rahela creates earth-toned, ready-to-wear fashion using Ethiopian fabrics. 
-                           Each piece reflects cultural heritage, sustainability, and modern comfort.</p>
-                        </div>
-                        </center>
-
-                  <div className="card-containers text-[black] text-center flex mt-[70px] justify-center gap-5">
-                        
-                        <div className="each-card transition-all duration-700 border-b-2 border-[#FEA489] h-[560px] w-[400px] shadow-[0_0_6px_#e7e5e5]  bg-[white] ">
-                              <div className="img overflow-hidden h-[380px]">
-                                    <Image src="/assets/card1.jpg" alt="about-image" width={400} height={200} className="h-[380px] object-cover "/>
-                              </div>
-
-                              <div className="about-description pt-[8px] ">
-                                    <h1 className="text-[25px]">Cultural Fashion Design</h1>
-                                    <p className="text-[#575757]">Rahela blends African tribal patterns with modern silhouettes.
-                                          Each design celebrates identity and connects heritage with style.</p>
-                                    <button className="w-[250px] h-[40px] mt-[10px] text-white cursor-pointer bg-[#FEA489] transition-all duration-500 hover:bg-[#864737]">Learn More</button>
-                              </div>
-                        </div>
-
-                        
-                                                <div className="each-card h-[560px] border-b-2 border-[#FEA489] w-[400px] shadow-[0_0_6px_#e7e5e5] bg-[white] ">
-                              <div className="img overflow-hidden h-[380px]">
-                                    <Image src="/assets/card.jpg" alt="about-image" width={400} height={200} className="h-[380px] object-cover"/>
-                              </div>
-
-                              <div className="about-description pt-[8px] ">
-                                    <h1 className="text-[25px]">Sustainable Craftsmanship</h1>
-                                    <p className="text-[#575757]">We use Ethiopian handwoven cotton and local materials.
-                                          Our process reduces waste and supports ethical fashion.</p>
-                                    <button className="w-[250px] h-[40px] mt-[10px] text-white cursor-pointer bg-[#FEA489] transition-all duration-500 hover:bg-[#864737]">Learn More</button>
-                              </div>
-                        </div>
-
-
-
-                                                <div className="each-card h-[560px] border-b-2 border-[#FEA489] w-[400px] shadow-[0_0_6px_#e7e5e5] bg-[white] ">
-                              <div className="img overflow-hidden h-[380px]">
-                                    <Image src="/assets/card2.jpg" alt="about-image" width={400} height={200} className="h-[380px] object-cover"/>
-                              </div>
-
-                              <div className="about-description pt-[8px] ">
-                                    <h1 className="text-[25px]">Ready-to-Wear Collections</h1>
-                                    <p className="text-[#575757]">Versatile, earth toned outfits designed for everyday comfort.Fashion that&apos;s globally
-                                          appealing yet deeply Afrocentric.</p>
-                                    <button className="w-[250px] h-[40px] mt-[10px] text-white cursor-pointer bg-[#FEA489] transition-all duration-500 hover:bg-[#864737]">Learn More</button>
-                              </div>
-                        </div>
-
-                        </div>
-
-
+                  </div>
             </div>
       
       </>
